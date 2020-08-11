92b20bcbbf715cee82d2170b4a98604352c8120e
carlosjunod <carlos@junod.cl>
2020-08-11 02:23:35 -0400

    Updates

---
+++ precache-manifest.6abf3578227eca4bc28b160380e47b19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b45bf886b0f6b16deea84233e650a57",
+    "url": "/Promiseint/index.html"
+  },
+  {
+    "revision": "84700f03827d803f5408",
+    "url": "/Promiseint/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d5bb925718179280a85",
+    "url": "/Promiseint/static/js/2.a572d1c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Promiseint/static/js/2.a572d1c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84700f03827d803f5408",
+    "url": "/Promiseint/static/js/main.9757b2c3.chunk.js"
+  },
+  {
+    "revision": "a95dd2e6d021921f87c5",
+    "url": "/Promiseint/static/js/runtime-main.65bebf14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Promiseint/static/media/logo.5d5d9eef.svg"
+  }
+]);
